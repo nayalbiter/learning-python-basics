e028763a6ef2f1b2dfef87d3943f5cd7aa7db83a
Carmen Albiter <98794975+nayalbiter@users.noreply.github.com>
2022-02-14 09:39:32 -0800

    Add files via upload

---
+++ multiples_of_3or5_iteration.py
@@ -0,0 +1,9 @@
+#Iteration 1:
+
+#Print all the numbers below 10 (> 0) that are multiples of 3 or 5 (this should print 3, 5, 6, and 9).
+
+for i in range (1,10):
+   if i% 3 == 0 or i% 5 ==0:
+        print(i)
+
+
