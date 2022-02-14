ce87ce84d8cb9680617a8be1321e105e6a9a7b26
Carmen Albiter <98794975+nayalbiter@users.noreply.github.com>
2022-02-14 09:35:14 -0800

    Add files via upload

---
+++ age_to_get_driver_license.py
@@ -0,0 +1,8 @@
+print("How old are you?: ")
+age = int (input ())
+
+if age >= 16:
+   print("You can get a driver’s license at this age")
+
+else:
+   print("You cannot get a driver’s license at this age")
